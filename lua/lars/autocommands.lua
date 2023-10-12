50c95e093e66f5a8195d6cfd41c86d9d69bffd80
Christian Wieland <cwieland@exocad.com>
2023-10-12 19:13:20 +0200

    auto update files changed outside of nvim

--- lua/lars/autocommands.lua
+++ lua/lars/autocommands.lua
@@ -5,3 +5,9 @@ local colorize = function()
 end
 local group = vim.api.nvim_create_augroup("Colors", {})
 vim.api.nvim_create_autocmd("ColorScheme", { callback = colorize, group = group, })
+
+vim.o.autoread = true
+vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "CursorHoldI", "FocusGained" }, {
+  command = "if mode() != 'c' | checktime | endif",
+  pattern = { "*" },
+})
