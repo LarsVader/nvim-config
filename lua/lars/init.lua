26b4a628936c263a45f7c4c484c44f9ba981102c
Christian Wieland <cwieland@exocad.com>
2023-10-12 19:07:10 +0200

    add user command to cd to git root of current file

--- lua/lars/init.lua
+++ lua/lars/init.lua
@@ -2,3 +2,13 @@ require("lars.autocommands")
 require("lars.keymap")
 require("lars.options")
 require("lars.plugin-load")
+
+
+local function get_git_root()
+    local dot_git_path = vim.fn.finddir(".git", ".;")
+    return vim.fn.fnamemodify(dot_git_path, ":h")
+end
+
+vim.api.nvim_create_user_command("CdGitRoot", function()
+    vim.api.nvim_set_current_dir(get_git_root())
+end, {})
