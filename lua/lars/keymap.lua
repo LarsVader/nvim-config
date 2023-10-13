353872e367f25bb696e37cf64d096f2e6dc26a84
Christian Wieland <cwieland@exocad.com>
2023-10-13 11:24:12 +0200

    abort dispatch to close running dentalcad when using keymap to run again

--- lua/lars/keymap.lua
+++ lua/lars/keymap.lua
@@ -13,12 +13,12 @@ vim.keymap.set({'n', 'v'}, 'p', 'p==', {})
 
 -- vim.keymap.set('n', '<leader>rr', [[:! for /F "TOKENS=1,2,*" \%a in ('tasklist /FI "IMAGENAME eq WpfApp2.exe"') do set MyPID=\%b<cr>]], {})
 
-vim.keymap.set('n', '<leader>md', [[<cmd>wall<cr><cmd>compiler! msbuild<cr><cmd>Make ./DentalCad/DentalCADWithDentalBase.sln /target:DentalCADApp /p:Configuration=DebugNet6 /p:Platform=x64 /filelogger /fileloggerparameters:verbosity=normal && "DentalCAD\\DentalCADApp\\bin\\x64\\Debug\\net6.0-windows\\DentalCADApp.exe" /DebugShowConsole /DebugShowConsoleKeepTrace /maik++ <cr>]], {})
-vim.keymap.set('n', '<leader>mr', [[<cmd>wall<cr><cmd>compiler! msbuild<cr><cmd>Make ./DentalCad/DentalCADWithDentalBase.sln /target:DentalCADApp /p:Configuration=ReleaseNet6 /p:Platform=x64 /filelogger /fileloggerparameters:verbosity=normal && "DentalCAD\\DentalCADApp\\bin\\x64\\Release\\net6.0-windows\\DentalCADApp.exe" /DebugShowConsole /DebugShowConsoleKeepTrace /maik++ <cr>]], {})
+vim.keymap.set('n', '<leader>md', [[<cmd>AbortDispatch<cr><cmd>wall<cr><cmd>compiler! msbuild<cr><cmd>Make ./DentalCad/DentalCADWithDentalBase.sln /target:DentalCADApp /p:Configuration=DebugNet6 /p:Platform=x64 /filelogger /fileloggerparameters:verbosity=normal && "DentalCAD\\DentalCADApp\\bin\\x64\\Debug\\net6.0-windows\\DentalCADApp.exe" /DebugShowConsole /DebugShowConsoleKeepTrace /maik++ <cr>]], {})
+vim.keymap.set('n', '<leader>mr', [[<cmd>AbortDispatch<cr><cmd>wall<cr><cmd>compiler! msbuild<cr><cmd>Make ./DentalCad/DentalCADWithDentalBase.sln /target:DentalCADApp /p:Configuration=ReleaseNet6 /p:Platform=x64 /filelogger /fileloggerparameters:verbosity=normal && "DentalCAD\\DentalCADApp\\bin\\x64\\Release\\net6.0-windows\\DentalCADApp.exe" /DebugShowConsole /DebugShowConsoleKeepTrace /maik++ <cr>]], {})
 
 
-vim.keymap.set('n', '<leader>rd', ':Dispatch DentalCAD\\DentalCADApp\\bin\\x64\\Debug\\net6.0-windows\\DentalCADApp.exe /DebugShowConsole /DebugShowConsoleKeepTrace /maik++ /DuplicateDentureMode=true<cr>', {})
-vim.keymap.set('n', '<leader>rr', ':Dispatch DentalCAD\\DentalCADApp\\bin\\x64\\Release\\net6.0-windows\\DentalCADApp.exe /DebugShowConsole /DebugShowConsoleKeepTrace /maik++ <cr>', {})
+vim.keymap.set('n', '<leader>rd', '<cmd>AbortDispatch<cr><cmd>Dispatch DentalCAD\\DentalCADApp\\bin\\x64\\Debug\\net6.0-windows\\DentalCADApp.exe /DebugShowConsole /DebugShowConsoleKeepTrace /maik++ /DuplicateDentureMode=true<cr>', {})
+vim.keymap.set('n', '<leader>rr', '<cmd>AbortDispatch<cr><cmd>Dispatch DentalCAD\\DentalCADApp\\bin\\x64\\Release\\net6.0-windows\\DentalCADApp.exe /DebugShowConsole /DebugShowConsoleKeepTrace /maik++ <cr>', {})
 
 vim.keymap.set('n', '<leader>k', function () vim.diagnostic.open_float() end)
 
