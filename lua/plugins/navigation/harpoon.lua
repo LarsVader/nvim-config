e072155e7847a201d7aa07c50255f542c8393b45
Christian Wieland <cwieland@exocad.com>
2023-10-19 11:38:08 +0200

    make harpoon window larger

--- lua/plugins/navigation/harpoon.lua
+++ lua/plugins/navigation/harpoon.lua
@@ -3,6 +3,11 @@ return {
 		-- pin buffers and jump to them fast
 		'ThePrimeagen/harpoon',
 		dependencies = { 'nvim-lua/plenary.nvim' },
+		opts = {
+			menu = {
+				width = vim.api.nvim_win_get_width(0) - 20,
+			}
+		},
 		keys = {
 			{"<leader>ha", function () require('harpoon.mark').add_file() end, desc='harpoon add'},
 			{"<leader>hh", function () require('harpoon.ui').toggle_quick_menu() end, desc='harpoon toggle quick menu'},
