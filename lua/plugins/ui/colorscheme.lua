9efff5d069f4eb6b05bdff1ae3c61675d98c9127
LarsVader <neo449@gmx.de>
2026-06-02 09:12:32 +0200

    feat(colorscheme): add eldritch.nvim with custom color overrides
    Adds eldritch.nvim as a new colorscheme option with a heavily customized
    palette (swapped reds/oranges/yellows to blues/cyans, transparent background)
    which comes from linkarzu
    and registers it in themery for live-preview switching.

--- lua/plugins/ui/colorscheme.lua
+++ lua/plugins/ui/colorscheme.lua
@@ -6,7 +6,73 @@ return {
 	{ 'rose-pine/neovim', name = 'rose-pine', lazy = true },
 	{ 'rebelot/kanagawa.nvim', lazy = true },
 	{ 'sainnhe/everforest', lazy = true },
-
+	{
+		"eldritch-theme/eldritch.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = {
+			transparent = true,
+			on_colors = function(global_colors)
+				-- Define all color overrides in a single table
+				local color_definitions = {
+					-- https://github.com/eldritch-theme/eldritch.nvim/blob/master/lua/eldritch/colors.lua
+					bg = "#0D1116",
+					fg = "#ebfafa",
+					selection = "#e9b3fd",
+					comment = "#a5afc2",
+					red = "#5fa9f4", -- default #f16c75
+					orange = "#1682ef", -- default #f7c67f
+					yellow = "#19dfcf", -- default #f1fc79
+					green = "#37f499",
+					purple = "#987afb", -- default #a48cf2
+					cyan = "#04d1f9",
+					pink = "#949ae5", -- default #f265b5
+					bright_red = "#5fa9f4",
+					bright_green = "#37f499",
+					bright_yellow = "#19dfcf",
+					bright_blue = "#987af",
+					bright_magenta = "#949ae5",
+					bright_cyan = "#04d1f9",
+					bright_white = "#ebfafa",
+					menu = "#0D1116",
+					visual = "#e9b3fd",
+					gutter_fg = "#e9b3fd",
+					nontext = "#e9b3fd",
+					white = "#ebfafa",
+					black = "#0D1116",
+					git = {
+						change = "#04d1f9",
+						add = "#37f499",
+						delete = "#f16c75",
+					},
+					gitSigns = {
+						change = "#04d1f9",
+						add = "#37f499",
+						delete = "#f16c75",
+					},
+					bg_dark = "#314154",
+					-- Lualine line across
+					bg_highlight = "#141b22",
+					terminal_black = "#314154",
+					fg_dark = "#ebfafa",
+					fg_gutter = "#314154",
+					dark3 = "#314154",
+					dark5 = "#314154",
+					bg_visual = "#e9b3fd",
+					dark_cyan = "#04d1f9",
+					magenta = "#949ae5",
+					magenta2 = "#949ae5",
+					magenta3 = "#949ae5",
+					dark_yellow = "#19dfcf",
+					dark_green = "#37f499",
+				}
+				-- Apply each color definition to global_colors
+				for key, value in pairs(color_definitions) do
+					global_colors[key] = value
+				end
+			end,
+		},
+	},
 	-- Theme switcher with persistence
 	{
 		'zaldih/themery.nvim',
@@ -19,6 +85,7 @@ return {
 			'rose-pine/neovim',
 			'rebelot/kanagawa.nvim',
 			'sainnhe/everforest',
+			'eldritch-theme/eldritch.nvim',
 		},
 		keys = {
 			{ "<leader>ut", "<cmd>Themery<cr>", desc = "Theme switcher" },
@@ -63,6 +130,7 @@ return {
 					{ name = "Kanagawa Lotus", colorscheme = "kanagawa-lotus" },
 					-- Everforest
 					{ name = "Everforest", colorscheme = "everforest" },
+					{ name = "linkarzu eldritch", colorscheme = "eldritch" },
 				},
 				livePreview = true,
 			})
